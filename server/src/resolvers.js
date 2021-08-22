834c4975fa8202024be782c8b45361515a5bd6cf
YiroYi <yiroyi@live.com>
2021-08-23 00:21:55 +0900

    Adding authors function to query authors data for each book

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,10 +1,14 @@
 import { allBooks, imageUrl } from "./book";
+import { authorsByBookId } from "./author";
 
 const resolvers = {
   Book: {
     imageUrl: (book, {size}) => {
       return imageUrl(size, book.googleId);
     },
+    authors: book => {
+      return authorsByBookId(book.id);
+    }
   },
   Query: {
     books: () => {
